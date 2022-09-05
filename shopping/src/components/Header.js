fd5f1ad580545d5de177c9e7f72511e6aa93df0c
Rlaur2 <plaureanopena@gmail.com>
2022-09-04 21:56:01 -0400

    Add and implement SVGs for the link icons

--- shopping/src/components/Header.js
+++ shopping/src/components/Header.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import './stylesheets/Header.css'
+import {ReactComponent as HomeIcon} from '/home/pavel/Projects/Shopping/shopping/src/images/home-heart.svg'
+import {ReactComponent as ShopIcon} from '/home/pavel/Projects/Shopping/shopping/src/images/shopping-outline.svg'
+import {ReactComponent as CartIcon} from '/home/pavel/Projects/Shopping/shopping/src/images/cart-outline.svg'
 
 export const Header = () => {
   return (
     <header>
         <div className="store-logo">Online Retailer</div>
         <ul>
-            <li>Home</li>
-            <li>Shop</li>
-            <li>Cart</li>
+            <li className='home'>Home <div className='home-icon'><HomeIcon /></div></li>
+            <li className='shop'>Shop <div className='shop-icon'><ShopIcon /></div></li>
+            <li className='cart'>Cart <div className='cart-icon'><CartIcon /></div></li>
         </ul>
     </header>
   )
