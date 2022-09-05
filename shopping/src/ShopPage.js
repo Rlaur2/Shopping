d2d128d84b98ec55ce6517ddb8caa41588d70ffa
Rlaur2 <plaureanopena@gmail.com>
2022-09-05 01:31:50 -0400

    Add routing to opening page and shop page

--- shopping/src/ShopPage.js
+++ shopping/src/ShopPage.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Header } from './components/Header'
 
 export const ShopPage = () => {
   return (
-    <div>ShopPage</div>
+    <div>
+      <Header />
+    </div>
   )
 }
--- shopping/src/components/Header.js
+++ shopping/src/components/Header.js
@@ -3,14 +3,15 @@ import './stylesheets/Header.css'
 import {ReactComponent as HomeIcon} from '/home/pavel/Projects/Shopping/shopping/src/images/home-heart.svg'
 import {ReactComponent as ShopIcon} from '/home/pavel/Projects/Shopping/shopping/src/images/shopping-outline.svg'
 import {ReactComponent as CartIcon} from '/home/pavel/Projects/Shopping/shopping/src/images/cart-outline.svg'
+import { Link } from 'react-router-dom'
 
 export const Header = () => {
   return (
     <header>
-        <div className="store-logo">Online Retailer</div>
+        <Link to={"/"}><div className="store-logo">Online Retailer</div></Link>
         <ul>
-            <li className='home'>Home <div className='home-icon'><HomeIcon /></div></li>
-            <li className='shop'>Shop <div className='shop-icon'><ShopIcon /></div></li>
+            <Link to={"/"}><li className='home'>Home <div className='home-icon'><HomeIcon /></div></li></Link>
+            <Link to={"/shop"}><li className='shop'>Shop <div className='shop-icon'><ShopIcon /></div></li></Link>
             <li className='cart'>Cart <div className='cart-icon'><CartIcon /></div></li>
         </ul>
     </header>
