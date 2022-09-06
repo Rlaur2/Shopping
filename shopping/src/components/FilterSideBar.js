72493f8bf3449d5a359ac85b8c1f6d21aac3de8a
Rlaur2 <plaureanopena@gmail.com>
2022-09-06 02:44:57 -0400

    Add filter price component

--- shopping/src/components/FilterSideBar.js
+++ shopping/src/components/FilterSideBar.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { FilterCategory } from './FilterCategory'
+import { FilterPrice } from './FilterPrice'
 
 export const FilterSideBar = () => {
   return (
     <div>
         <FilterCategory />
+        <FilterPrice />
     </div>
   )
 }
