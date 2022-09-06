9a929d57d5ef7b268ed28ace9526a272b2c1f380
Rlaur2 <plaureanopena@gmail.com>
2022-09-05 20:35:10 -0400

    Complete styling of product card

--- shopping/src/components/Product.js
+++ shopping/src/components/Product.js
@@ -5,8 +5,10 @@ export const Product = ({category, description, image, price, rating, title}) =>
   return (
     <div className='product'>
         <img src={image} alt={title} />
-        <div>{title}</div>
-        <div>${price}</div>
+        <div className='product-text'>
+            <h3 className='title'>{title}</h3>
+            <p className='price'>${price}</p>
+        </div>
     </div>
   )
 }
