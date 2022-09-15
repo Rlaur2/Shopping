ffde7c5154ed34d93b2b4e58c079b4001d9cbc1a
Rlaur2 <plaureanopena@gmail.com>
2022-09-15 03:17:59 -0400

    Try HashRouter instead of BrowserRouter

--- shopping/src/RouteSwitch.js
+++ shopping/src/RouteSwitch.js
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import { ShopPage } from "./ShopPage";
 import { ProductInfo } from "./components/ProductInfo";
 
 export const RouteSwitch = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL} >
+    <HashRouter>
             <Routes>
                 <Route path="/" element={<App />}/>
                 <Route path="/shop" element={<ShopPage />}/>
                 <Route path="/shop/:id" element={<ProductInfo/>}/>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
   )
 }
