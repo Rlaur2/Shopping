ff5cc3c1e6ae49ea4668fdcedcc17e1bcf153d48
Rlaur2 <plaureanopena@gmail.com>
2022-09-15 03:20:30 -0400

    Revert to BrowserRouter

--- shopping/src/RouteSwitch.js
+++ shopping/src/RouteSwitch.js
@@ -1,16 +1,16 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import { ShopPage } from "./ShopPage";
 import { ProductInfo } from "./components/ProductInfo";
 
 export const RouteSwitch = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
             <Routes>
                 <Route path="/" element={<App />}/>
                 <Route path="/shop" element={<ShopPage />}/>
                 <Route path="/shop/:id" element={<ProductInfo/>}/>
             </Routes>
-        </HashRouter>
+    </BrowserRouter>
   )
 }
